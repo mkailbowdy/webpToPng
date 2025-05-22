ae147cb1f7797e21ad6d8dcfe054d8f5f6923fa4
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2025-05-22 13:20:45 +0900

    add option for jpeg conversion

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"golang.org/x/image/webp"
+	"image/jpeg"
 	"image/png"
 	_ "image/png"
 	"log"
@@ -12,17 +15,26 @@ import (
 )
 
 func main() {
+	mime := flag.String("mime", "jpeg", "MIME type to convert to")
+	q := flag.Int("q", 100, "Quality of image")
+	flag.Parse()
+	fmt.Printf("Mime type: %s\n", *mime)
+	options := jpeg.Options{Quality: *q}
 	// Get directory user's system
 	home, err := os.UserHomeDir()
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 	subdir := os.Args[1:]
-	fullPath := filepath.Join(home, subdir[0])
+	fmt.Printf("subdir path: %s\n", subdir)
+
+	fullPath := filepath.Join(home, subdir[1])
 
 	// Get all webp files in the directory
 	files, err := filepath.Glob(fullPath + "/*.webp")
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 
@@ -31,22 +43,41 @@ func main() {
 		name := strings.TrimSuffix(filepath.Base(file), ".webp")
 		img, err := os.Open(file)
 		if err != nil {
+			fmt.Println(err)
 			log.Fatal(err)
 		}
 
 		// Decode the webp data, then create a new file
 		webpImg, err := webp.Decode(img)
-		newFile, err := os.Create(fullPath + "/" + name + strconv.Itoa(i) + ".png")
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		// encode the data into png and write it to the new file
-		err = png.Encode(newFile, webpImg)
-		if err != nil {
-			log.Fatal(err)
-		}
+		var newFile *os.File
+		if *mime == "jpeg" {
+			log.Printf("jpeg")
+			newFile, err = os.Create(fullPath + "/" + name + strconv.Itoa(i) + ".jpg")
+			err = jpeg.Encode(newFile, webpImg, &options)
+			if err != nil {
+				fmt.Println(err)
 
+				log.Fatal(err)
+			}
+		} else {
+			log.Printf("png")
+			newFile, err = os.Create(fullPath + "/" + name + strconv.Itoa(i) + ".png")
+			if err != nil {
+				fmt.Println(err)
+
+				log.Fatal(err)
+			}
+
+			// encode the data into png and write it to the new file
+			err = png.Encode(newFile, webpImg)
+			if err != nil {
+				fmt.Println(err)
+
+				log.Fatal(err)
+			}
+
+		}
 		img.Close()
 	}
 }
